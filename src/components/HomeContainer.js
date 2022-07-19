bf416d498736965d8e52763ea86b40ddcee811f6
ypling <leonard.li.pub@gmail.com>
2022-07-19 14:20:37 -0700

    set pending

--- src/components/HomeContainer.js
+++ src/components/HomeContainer.js
@@ -2,21 +2,27 @@ import React, { useEffect, useState } from 'react'
 import { Grid } from '@mui/material'
 import UserAmountView from './UserAmountView'
 import axios from 'axios'
+import { CircularProgress } from '@mui/material'
+import Box from '@mui/material/Box'
 
 const HomeContainer = () => {
   const [amountData, setAmountData] = useState([])
+  const [isPending, setIsPending] = useState(true)
 
   useEffect(() => {
     axios.get('https://kids-bank-ypling.herokuapp.com/amount').then(res => {
       res.data[0].bgImgSrc = 'https://wallpaper.dog/large/20517298.jpg'
       res.data[1].bgImgSrc = 'https://wallpaperaccess.com/full/477608.jpg'
       setAmountData(res.data)
+      setIsPending(false)
     })
   },[])
 
 
   return (
-    <Grid container direction="column" spacing={1}>
+    <Box sx={{ display: 'flex', alignItems: 'center', justifyContent:'center'}}>
+      {isPending && <CircularProgress />}
+      {!isPending && <Grid container direction="column" spacing={1}>
       {amountData.map(amountObj => (
         <UserAmountView
           key={amountObj.id}
@@ -25,7 +31,10 @@ const HomeContainer = () => {
           bgImgSrc={amountObj.bgImgSrc}
         />
       ))}
-    </Grid>
+      </Grid>}
+
+    </Box>
+
   )
 }
 
